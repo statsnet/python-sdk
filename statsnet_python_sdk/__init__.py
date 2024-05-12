ae1369ef8f145ae09862f80b30f736fbc082991b
kirillinakin <kirillinyakin@yandex.ru>
2024-05-12 14:27:51 +0300

    Prepare for tag

--- statsnet_python_sdk/__init__.py
+++ statsnet_python_sdk/__init__.py
@@ -2,4 +2,4 @@ from .client import Client
 from .exceptions import ClientException, InvalidParamsException
 
 __all__ = ["ClientException", "InvalidParamsException", "Client"]
-__version__ = "1.0.0"
+__version__ = "1.1.0"
